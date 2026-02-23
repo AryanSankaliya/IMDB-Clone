c423bf74952c426933ac1138bd9f43c8fcdf9cc3
Aryan Sankaliya <aryansankaliya@gmail.com>
2026-02-23 22:28:59 +0530

    fix: Correct casing in import statements for Movie component and CSS file

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import Header from './components/header/Header';
 import Home from './pages/home/Home';
 import MovieList from './components/movieList/MovieList';
-import Movie from './pages/movieDetail/Movie';
+import Movie from './pages/movieDetail/movie';
 import Error from './pages/Error/Error'
 
 
